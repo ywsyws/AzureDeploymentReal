b4a27edbf38d6ffa33d2872b5dfcbe3b1f67b1e6
ywsyws <channing.platevoet@gmail.com>
2019-12-10 16:42:28 +0000

    put back hello world app

--- app.py
+++ app.py
@@ -1,48 +1,10 @@
-import io
-import pandas as pd
-import pyodbc
 from flask import Flask
-from flask import render_template
-from flask import Response
-from graph import create_figure
-from matplotlib.backends.backend_agg import FigureCanvasAgg
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('home.html', name='Channing!')
-
-
-@app.route('/plot.png')
-def plot():
-
-    conn = pyodbc.connect(
-        'Driver={ODBC Driver 17 for SQL Server};'
-        'Server=azuresqlorange.database.windows.net;'
-        'Database=orange_azure;'
-        'Trusted_Connectoin=yes;'
-        'UID=orange;'
-        'PWD=Supermotdepasse!42;')
-    cursor = conn.cursor()
-
-    query = """SELECT year, COUNT(year) AS count
-            FROM analysis_movies
-            GROUP BY year
-            """
-    df = pd.read_sql(query, conn)
-
-    fig = create_figure(df)
-
-    output = io.BytesIO()
-    FigureCanvasAgg(fig).print_png(output)
-    return Response(output.getvalue(), mimetype='image/png')
-
-
-@app.route('/graph')
-def graph():
-    return render_template("hello.html", Test="Channing!")
-
+    return "<h1>Hello Azure!</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
