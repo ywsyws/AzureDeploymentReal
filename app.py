7879a123edef2de41e0484a990e75130bda542fc
ywsyws <channing.platevoet@gmail.com>
2019-12-11 13:48:21 +0100

    adddd 2nd page

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello Azure!</h1>"
+    return render_template('home.html', name='Channing!')
+
+@app.route('/graph')
+def graph():
+    return render_template("hello.html", Test="Channing!")
 
 if __name__ == '__main__':
     app.run(debug=True)
