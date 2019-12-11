68285ec9b665b21505a940962dd6df128f87fdc3
ywsyws <channing.platevoet@gmail.com>
2019-12-11 15:03:25 +0100

    add comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# import libraries
 import io
 import pandas as pd
 import pyodbc
@@ -6,17 +7,29 @@ from flask import render_template
 from flask import Response
 from graph import create_figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+# Create an instance of the Flask class
 app = Flask(__name__)
 
-
+# Homepage of the web app
 @app.route('/')
 def index():
     return render_template('home.html', name='Channing!')
 
-
+# Hello page to show the graph using the result of a SQL query
 @app.route('/plot.png')
 def plot():
+    """ SQL query and plot graph
 
+    Using the connect function of pyodbc to connect to 
+    the Azure SQL Database and
+    making a query.
+
+    Then using the query result to plot a graph.
+    This graph will be used by the hello.html.
+    """
+
+    # Connect to the Azure SQL database
     conn = pyodbc.connect(
         'Driver={ODBC Driver 17 for SQL Server};'
         'Server=azuresqlorange.database.windows.net;'
@@ -26,19 +39,22 @@ def plot():
         'PWD=Supermotdepasse!42;')
     cursor = conn.cursor()
 
+    # Making the SQL query
     query = """SELECT year, COUNT(year) AS count
             FROM analysis_movies
             GROUP BY year
             """
+    # Putting the result in a dataframe
     df = pd.read_sql(query, conn)
 
+    # Call the create_figure method from graph.py to plot a graph
     fig = create_figure(df)
 
     output = io.BytesIO()
     FigureCanvasAgg(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
-
+# Hello page to show the graph using the result of a SQL query
 @app.route('/graph')
 def graph():
     return render_template("hello.html", Test="Channing!")
