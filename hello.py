9b088653c52564728ce6c6e0e9a40076e17a5d88
ywsyws <channing.platevoet@gmail.com>
2019-12-14 23:55:06 +0100

    modify comment.html

--- hello.py
+++ hello.py
@@ -83,8 +83,8 @@ def comment():
         user_id = form.user_id.data
         movie_id = form.movie_id.data
         comment = form.comment.data
-        form.user_id.data = ''
-        form.movie_id.data = ''
+        form.user_id.data = None
+        form.movie_id.data = None
         form.comment.data = ''
     return render_template('comment.html', form=form, user_id=user_id,movie_id=movie_id, comment=comment, name='Channing!')
 
